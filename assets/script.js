17f8aae97fc6fcd5be605859bc1fd9a785925993
BONAFIDE BOSS <contact@almir.info>
2024-02-15 19:47:53 +0400

    added the sort functionality

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,25 @@ var app = new Vue({
     cart: [],
     showCheckout: false,
     lessons: lessons,
+    sortBy: "subject",
+    sortOrderAsc: true,
   },
-  methods: {},
-  computed: {},
+  computed: {
+    sortedLessons() {
+      const attribute = this.sortBy;
+      const order = this.sortOrderAsc ? 1 : -1;
+
+      function compare(a, b) {
+        if (a[attribute] < b[attribute]) return -1 * order;
+        if (a[attribute] > b[attribute]) return 1 * order;
+        return 0;
+      }
+      return this.lessons.sort(compare);
+    },
+  },
+  methods: {
+    toggleSortOrder() {
+      this.sortOrderAsc = !this.sortOrderAsc;
+    },
+  },
 });
